b289c8ef1a94298e37b2789e0e49e0767acb5cd9
maxime <maxime@genohm.com>
2025-05-01 20:56:07 +0200

    All actions and proper handling of composite extensions

---
+++ src/main/kotlin/ch/namlin/angularswitchershortcuts/action/SwitchActions.kt
@@ -0,0 +1,48 @@
+package ch.namlin.angularswitchershortcuts.action
+
+import ch.namlin.angularswitchershortcuts.util.ExtensionSwitcher
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.CommonDataKeys.EDITOR
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+
+
+abstract class SwitchAction : AnAction() {
+
+    override fun update(event: AnActionEvent) {
+        val project: Project? = event.project
+        val editor: Editor? = event.getData(EDITOR)
+        event.presentation.isEnabledAndVisible = (project != null && editor != null)
+    }
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val editor: Editor? = event.getData(CommonDataKeys.EDITOR)
+        if (editor == null || event.project == null) return
+        doSwitch(ExtensionSwitcher(event.project!!, editor))
+    }
+
+    abstract fun doSwitch(switcher: ExtensionSwitcher)
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.EDT
+    }
+}
+
+class SwitchToTemplate : SwitchAction() {
+    override fun doSwitch(switcher: ExtensionSwitcher) = switcher.switchToTemplate()
+}
+
+class SwitchToComponent : SwitchAction() {
+    override fun doSwitch(switcher: ExtensionSwitcher) = switcher.switchToComponent()
+}
+
+class SwitchToStyle : SwitchAction() {
+    override fun doSwitch(switcher: ExtensionSwitcher) = switcher.switchToStyle()
+}
+
+class SwitchToTest : SwitchAction() {
+    override fun doSwitch(switcher: ExtensionSwitcher) = switcher.switchToTest()
+}
--- src/main/kotlin/ch/namlin/angularswitchershortcuts/util/ExtensionSwitcher.kt
+++ src/main/kotlin/ch/namlin/angularswitchershortcuts/util/ExtensionSwitcher.kt
@@ -8,13 +8,14 @@ import com.intellij.openapi.vfs.VirtualFile
 import java.util.*
 
 class ExtensionSwitcher(private val project: Project, private val editor: Editor) {
+    private val testExtensions = listOf("spec.ts")
     private val templateExtensions = listOf("html", "svg")
     private val componentExtensions = listOf("ts")
-    private val testExtensions = listOf("spec.ts")
     private val styleExtensions = listOf("css", "scss", "less", "styl")
+    private val supportedExtensions = testExtensions + templateExtensions + componentExtensions  + styleExtensions
 
     fun switchToTemplate() {
-        switchToGroup()
+        switchToGroup(templateExtensions)
     }
 
     fun switchToComponent() {
@@ -41,7 +42,13 @@ class ExtensionSwitcher(private val project: Project, private val editor: Editor
     }
 
     private val currentBasePath: String?
-        get() = editor.virtualFile?.let { it.canonicalPath?.replace(it.name, it.nameWithoutExtension)} ?: throw BasePathNotFoundException()
+        get() {
+            var path = editor.virtualFile?.canonicalPath ?: throw BasePathNotFoundException()
+            supportedExtensions.forEach { extension: String ->
+                path = path.replace(".$extension", "")
+            }
+            return path
+        }
 
     private fun findFileByPath(path: String): Optional<VirtualFile> {
         return Optional.ofNullable<VirtualFile>(LocalFileSystem.getInstance().findFileByPath(path))
