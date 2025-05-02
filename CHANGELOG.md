20573e0ff221bcd939a2cbf4b235a19f3b864adc
maxime <maxime@genohm.com>
2025-05-02 20:21:04 +0200

    Fix changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## [Unreleased]
+
+## [1.0.0]
+
+### Added
+
+- Initial project scaffold
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
@@ -52,6 +53,19 @@ intellijPlatform {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
                 subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+            }
+        }
+
+        val changelog = project.changelog // local variable for configuration cache compatibility
+        // Get the latest available change notes from the changelog file
+        changeNotes = providers.gradleProperty("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
             }
         }
     }
